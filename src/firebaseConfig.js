28cc9c697998be09c6ed5e7cec69d83fe8ba8523
Juninhorezini <juninhorezini@gmail.com>
2026-08-26 18:13:20 -0300

    chore: refactor configuration and scripts
    - Secure Firebase credentials using environment variables
    - Add development and linting scripts
    - Update homepage path and project metadata

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -24,13 +24,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJYlIedpzXb0zUOd75caRZcSdXHPfymjQ",
-  authDomain: "controlstockapp-538ba.firebaseapp.com",
-  databaseURL: "https://controlstockapp-538ba-default-rtdb.firebaseio.com",
-  projectId: "controlstockapp-538ba",
-  storageBucket: "controlstockapp-538ba.firebasestorage.app",
-  messagingSenderId: "262890644963",
-  appId: "1:262890644963:web:8be72918d199896246acd6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAJYlIedpzXb0zUOd75caRZcSdXHPfymjQ",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "controlstockapp-538ba.firebaseapp.com",
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || "https://controlstockapp-538ba-default-rtdb.firebaseio.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "controlstockapp-538ba",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "controlstockapp-538ba.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "262890644963",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:262890644963:web:8be72918d199896246acd6"
 };
 
 const app = initializeApp(firebaseConfig);
